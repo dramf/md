1ef35694a1dca743f22bea5e1b7c06ddd1284573
olsova <olga.solopova@smartforce.io>
2024-11-01 13:34:31 +0600

    feat: print trivy output (#2)
    * feat: print trivy output
    
    * refactor: remove .idea
    
    * feat: add macos platform

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
-package md
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	inputData, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatalf("Error reading from stdin: %v\n", err)
+	}
+
+	fmt.Printf("Trivy output data: %s\n", inputData)
+}
